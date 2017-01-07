28f3e43293c2b7a1059ac8f61e31492158e812d6
Spencer <spenc@LAPTOP-VO6GPSQV>
2017-01-06 22:01:32 -0600

    Better list with question object

--- QofD/QofD/Index.xaml.cs
+++ QofD/QofD/Index.xaml.cs
@@ -44,20 +44,29 @@ namespace QofD
             questions.Add("Is Fiji water really just rich people spit?");
             questions.Add("Are Burt and Ernie from California?");
 
+            List<QuestionItem> questSource = new List<QuestionItem>
+            {
+                new QuestionItem("How do you eat a porcupine safely?",new DateTime(2016,8,16,2,3,56),true),
+                new QuestionItem("How many dogs does it take to make a pancake?",new DateTime(2016,12,12,12,35,56),true),
+                new QuestionItem("Is Fiji water really just rich people spit?",new DateTime(2017,1,1,1,1,1),true),
+                new QuestionItem("Are Burt and Ernie from California?",new DateTime(2017,1,6,3,56,56),true)
+            };
+
             ListView questionView = new ListView
             {
                 SeparatorColor = Color.Gray,
-                ItemsSource = questions,
+                ItemsSource = questSource,
+                VerticalOptions = LayoutOptions.Center,
                 ItemTemplate = new DataTemplate(() =>
                 {
                     // Create views with bindings for displaying each property.
                     Label questionLabel = new Label();
                     questionLabel.SetBinding(Label.TextProperty, "Question");
+                    questionLabel.FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label));
 
                     Label createdLabel = new Label();
-                    createdLabel.SetBinding(Label.TextProperty,
-                        new Binding("Asked", BindingMode.OneWay,
-                            null, null, "first {0:d}"));
+                    createdLabel.SetBinding(Label.TextProperty, "Created");
+                    createdLabel.FontSize = Device.GetNamedSize(NamedSize.Micro, typeof(Label));
 
                     // Return an assembled ViewCell.
                     return new ViewCell
@@ -70,7 +79,8 @@ namespace QofD
                                 {
                                     new StackLayout
                                     {
-                                        VerticalOptions = LayoutOptions.Center,
+                                        VerticalOptions = LayoutOptions.FillAndExpand,
+                                        HorizontalOptions = LayoutOptions.FillAndExpand,
                                         Spacing = 0,
                                         Children =
                                         {
