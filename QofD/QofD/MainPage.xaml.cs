ebca590e672090c2acd91a80cdb205c97b9d68ba
Spencer <spenc@LAPTOP-VO6GPSQV>
2017-01-11 13:26:12 -0600

    Save button and Handler

--- QofD/QofD/MainPage.xaml.cs
+++ QofD/QofD/MainPage.xaml.cs
@@ -22,29 +22,45 @@ namespace QofD
             Label greeting = new Label
             {
                 Text = "What question would you like to know?",
-                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+
             };
 
             Button toIndex = new Button
             {
                 Text = "Question List",
+
             };
             toIndex.Clicked += OnButtonClicked;
+
+            Button saver = new Button
+            {
+                Text = "Save"
+            };
+            saver.Clicked += HandleTouchUpInside;
+
+           
 
             Content = new StackLayout
             {
                 Children =
                 {
-                    greeting, question, toIndex
+                    greeting, question, toIndex, saver
                 }
             };
         }
 
         async private void OnButtonClicked(object sender, EventArgs e)
         {
+            System.Diagnostics.Debug.WriteLine(e.ToString());
             DateTime now = DateTime.Now.ToLocalTime();
             QuestionItem newEntry = new QuestionItem(question.Text, now, false);
             await Navigation.PushAsync(new Index(newEntry));
+        }
+
+        void HandleTouchUpInside(object sender, EventArgs e)
+        {
+            DisplayAlert("Alert", "Saved", "OK");
         }
     }
 }
